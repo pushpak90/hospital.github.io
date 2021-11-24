d29294c65b9272aa91dce0c9dd5888719396a6c5
pushpak90 <64779040+pushpak90@users.noreply.github.com>
2021-11-24 17:29:13 +0530

    Add files via upload

---
+++ firebaseconnectivity.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBZUHLrflTbMwD9Gw9Qung8-nyyjtQWraE",
+    authDomain: "fir-da4c4.firebaseapp.com",
+    databaseURL: "https://fir-da4c4-default-rtdb.firebaseio.com",
+    projectId: "fir-da4c4",
+    storageBucket: "fir-da4c4.appspot.com",
+    messagingSenderId: "597956611268",
+    appId: "1:597956611268:web:6ec7fffa7b0b5f0bebcd91"
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
